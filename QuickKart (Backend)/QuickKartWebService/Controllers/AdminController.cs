7395fedc31ac3643caaeff1f63be5734959b4653
Karthik N U <karthiknu01@gmail.com>
2026-03-27 03:17:08 +0530

    Add files via upload

---
+++ QuickKart (Backend)/QuickKartWebService/Controllers/AdminController.cs
@@ -0,0 +1,67 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickKartWebService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+                var fileName = file.FileName;
+                string url = "";
+                if (file.Length > 0)
+                {
+                    string connectionString = "DefaultEndpointsProtocol=https;AccountName=quickcartstorage;AccountKey=ka3p1LOkh6rM4cwBUv5/oDsmfccN2EF1lLF6YP09i618Nq4na2GuomcqZXuoQ8Mc4Ohcc0mD9Dbm+AStcGEfeQ==;EndpointSuffix=core.windows.net";
+                    string containerName = "data";
+                    BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+
+                    try
+                    {
+                        // Get a reference to a blob
+                        BlobClient blob = container.GetBlobClient(fileName);
+
+                        // Open the file and upload its data
+                        using (Stream file1 = file.OpenReadStream())
+                        {
+                            blob.Upload(file1);
+                        }
+
+                         url = blob.Uri.AbsoluteUri;
+                    }
+                    catch
+                    {
+                        return BadRequest();
+                    }
+
+                    return Ok(url);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+
+
+
+
+    }
+}
